c61ac4c9d355b3c6ed8c8d80e592796b3584800c
TimofeyVoronin <tima.voronin27@mail.ru>
2026-05-07 04:54:53 +0700

    BUD-349 Configure PostgreSQL connection through env

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,21 +1,24 @@
 from .base import *
 
 
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=True)
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS",
+    default=["localhost", "127.0.0.1"],
+)
 
 
 AUTH_PASSWORD_VALIDATORS = []
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOWED_ORIGINS = env.list(
+    "CORS_ALLOWED_ORIGINS",
+    default=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+)
 
 
 LOGGING = {
