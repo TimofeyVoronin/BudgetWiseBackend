a7b496b21a6619b4b3d09beaa6e17e1b3f7951b3
TimofeyVoronin <tima.voronin27@mail.ru>
2026-05-08 01:47:55 +0700

    BUD-383 Describe finance models in ORM

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Дата обновления",
+    )
+
+    class Meta:
+        abstract = True
